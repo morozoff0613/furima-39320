607c86c71caad9af02f0ddd61dab4d40fc26b266
morozoff0613 <morodome5@yahoo.co.jp>
2023-09-15 21:27:55 +0900

    ユーザー管理機能（修正1）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠]+\z/,
+    message: "は全角日本語で入力してください" }
+
+  # 名字(カナ)と名前(カナ)が全角カタカナであることを検証する正規表現
+  validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
+    message: "は全角カタカナで入力してください" }
+
+  # パスワードが英数字混合であることを検証する正規表現
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
+    message: "は英字と数字の両方を含む必要があります" }
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -11,6 +23,5 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
 
-  has_many :items
-  has_many :orders
+
 end
